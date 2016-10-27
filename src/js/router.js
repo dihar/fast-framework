3375e8165f8f40c3830485fd211032cfdf1b9b31
dihar <dharin@npoprogress.com>
2016-10-27 17:47:39 +0300

    add comment in router

--- src/js/router.js
+++ src/js/router.js
@@ -4,10 +4,13 @@
 (function(){
 
 	var getCont = ff.controller.get;
-	//here add routes (name, controller, title);
+	//here add routes (name, controller, {title, param, hashParams});
 	
 	ff.router.add('/', getCont('index'));
-	// ff.router.add('/request-form/:lot_id', getCont('request-form'), 'Форма заявки', 'lot_id');
+	// ff.router.add('/request-form/:lot_id', getCont('request-form'), {
+	// 		param: 'lot_id',
+	// 		title: 'Форма заявки'
+	// });
 
 })();
 
